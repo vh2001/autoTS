1cedf384c8721860ff83a36dea68eb580dcb610f
vHanzel <vid.hanzel@gmail.com>
2024-06-21 11:02:55 +0200

    updated config for InceptionTime

--- src/config.py
+++ src/config.py
@@ -1,19 +1,20 @@
 import json
 from datetime import datetime
 import os
-DATASET = "LQE"
-DATASET_PATH = "./data/LQE/dataset_LQE_uncut.csv"
+DATASET = "NILM" # select DATASTE for any NILM data set put NILM and then provide the direct path to the .pkl file
+DATASET_PATH = "../Energy_graph/energy-knowledge-graph/data/parsed/DRED.pkl"
 
-TRANSFORMATION = "GADF"
-MODEL = "vgg11"
+TRANSFORMATION = "GADF" # select transformation and keep in mind which transformation works with which model the available transformations can be seen in the transformation.py file
+MODEL = "VGG16" # select model the available models can be found on our github 
 TASK ="classification" # for now only classification is supported
 
 
-EXPERIMENT_NAME = f"{DATASET}_{TRANSFORMATION}_{MODEL}"
+EXPERIMENT_NAME = f"{DATASET}_{TRANSFORMATION}_{MODEL}" # name of expirement
 
 # add date to the experiment name
-EXPERIMENT_NAME = f"{EXPERIMENT_NAME}_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
-SAVE_PATH = f"./results/{EXPERIMENT_NAME}"
+EXPERIMENT_NAME = f"{EXPERIMENT_NAME}_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}" # we add the date to the experiment name by default this can be changed here
+
+SAVE_PATH = f"./results/{EXPERIMENT_NAME}" # save path where the results and model will be saved
 
 
 # create save folder if it doesnt exist
@@ -28,21 +29,45 @@ SHUFFLE = True # shuffle training data
 
 
 # model parameters
-EPOCHS = 10
+EPOCHS = 2
 LR = 0.01
 BATCH_SIZE = 32
 CALLBACKS = []
 
 
 # InceptionTime specific parameters (for more details read paper: https://arxiv.org/abs/1909.04939)
-INPUT_SHAPE = None # input shape for the InceptionTime model for eg. (300, 1) for 300 timesteps and 1 feature (univariate time series
-NUM_CLASSES = None # number of classes for classification
-DEPTH = None # depth of the InceptionTime model 
-KERNEL_SIZE = None # kernel size for the InceptionTime model
+INPUT_SHAPE = (299,1) # input shape for the InceptionTime model for eg. (300, 1) for 300 timesteps and 1 feature (univariate time series
+NUM_CLASSES = 5 # number of classes for classification
+DEPTH = None # depth of the InceptionTime model leave None for default(6)
+KERNEL_SIZE = None # kernel size for the InceptionTime model leave None for default(42)
 
 
+# function to save the used config
 def save_config():
-    cfg = {
+    
+    if MODEL == "InceptionTime":
+        cfg = {
+        "DATASET": DATASET,
+        "DATASET_PATH": DATASET_PATH,
+        "TRANSFORMATION": TRANSFORMATION,
+        "MODEL": MODEL,
+        "TASK": TASK,
+
+        "DATA_SPLIT": DATA_SPLIT,
+        "FOLDS": FOLDS,
+        "SHUFFLE": SHUFFLE,
+
+        "EPOCHS": EPOCHS,
+        "LR": LR,
+        "BATCH_SIZE": BATCH_SIZE,
+        "CALLBACKS": CALLBACKS,
+
+        "INPUT_SHAPE": INPUT_SHAPE,
+        "NUM_CLASSES": NUM_CLASSES,
+        "DEPTH": DEPTH,
+        "KERNEL_SIZE": KERNEL_SIZE
+    else:
+        cfg = {
         "DATASET": DATASET,
         "DATASET_PATH": DATASET_PATH,
         "TRANSFORMATION": TRANSFORMATION,
@@ -57,6 +82,10 @@ def save_config():
         "LR": LR,
         "BATCH_SIZE": BATCH_SIZE,
         "CALLBACKS": CALLBACKS
+    }
+            
+            
+        
     }
 
     with open(f"{SAVE_PATH}/config.json", "w") as f:
